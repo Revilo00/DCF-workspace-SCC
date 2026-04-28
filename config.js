b85c895981e6c64e3cd110cffd8a0890454c4094
Revilo00 <olivertyson@live.com.au>
2026-04-29 09:36:52 +1000

    feat: refactor to DTCG-compliant token structure with primitive/semantic tiers
    Adds Tokens Studio preprocessor and two-tier token architecture

--- config.js
+++ config.js
@@ -1,8 +1,33 @@
+import StyleDictionary from 'style-dictionary';
+import { register } from '@tokens-studio/sd-transforms';
+
+register(StyleDictionary);
+
 export default {
   source: ['src/tokens/**/*.json'],
+  preprocessors: ['tokens-studio'],
   platforms: {
-    css:  { transformGroup: 'css',  buildPath: 'build/css/',  files: [{ destination: 'variables.css',   format: 'css/variables' }] },
-    scss: { transformGroup: 'scss', buildPath: 'build/scss/', files: [{ destination: '_variables.scss', format: 'scss/variables' }] },
-    js:   { transformGroup: 'js',   buildPath: 'build/js/',   files: [{ destination: 'tokens.js',       format: 'javascript/es6' }] }
+    css: {
+      transformGroup: 'tokens-studio',
+      transforms: ['name/kebab'],
+      buildPath: 'build/css/',
+      files: [{
+        destination: 'variables.css',
+        format: 'css/variables',
+        options: { outputReferences: true }
+      }]
+    },
+    scss: {
+      transformGroup: 'tokens-studio',
+      transforms: ['name/kebab'],
+      buildPath: 'build/scss/',
+      files: [{ destination: '_variables.scss', format: 'scss/variables' }]
+    },
+    js: {
+      transformGroup: 'tokens-studio',
+      transforms: ['name/camel'],
+      buildPath: 'build/js/',
+      files: [{ destination: 'tokens.js', format: 'javascript/es6' }]
+    }
   }
 };
