a24b6972615c5a9414863e70adf2249c9353749f
Revilo00 <olivertyson@live.com.au>
2026-04-29 09:26:01 +1000

    feat: initial token pipeline with Style Dictionary

---
+++ config.js
@@ -0,0 +1,8 @@
+export default {
+  source: ['src/tokens/**/*.json'],
+  platforms: {
+    css:  { transformGroup: 'css',  buildPath: 'build/css/',  files: [{ destination: 'variables.css',   format: 'css/variables' }] },
+    scss: { transformGroup: 'scss', buildPath: 'build/scss/', files: [{ destination: '_variables.scss', format: 'scss/variables' }] },
+    js:   { transformGroup: 'js',   buildPath: 'build/js/',   files: [{ destination: 'tokens.js',       format: 'javascript/es6' }] }
+  }
+};
